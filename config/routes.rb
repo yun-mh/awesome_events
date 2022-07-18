b5d1a3bb540cf0d93a616bce11116ee0877e0c0d
Minho Yun <minhoyun.dev@gmail.com>
2022-07-18 11:21:22 +0900

    Implement participation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+  
+  # イベントとチケットリソースは親子関係
+  resources :events do
+    resources :tickets
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
