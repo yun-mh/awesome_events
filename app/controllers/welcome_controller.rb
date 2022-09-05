ed30eb51ddf3dfd64ebe2d121f999220b3703848
Minho Yun <minhoyun.dev@gmail.com>
2022-09-05 15:43:20 +0900

    Add kaminari for pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate # can be accessed even if not logged in 
 
   def index
-    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
+    @events = Event.page(params[:page]).per(10).where("start_at > ?", Time.zone.now).order(:start_at)
   end
 end
