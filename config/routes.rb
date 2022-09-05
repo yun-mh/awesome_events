49128cb70d4d2b750f1ef2d04f6976b4081610c0
Minho Yun <minhoyun.dev@gmail.com>
2022-09-05 17:46:19 +0900

    Add 404, 500 error handling

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,15 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
   
-  resource :retirements
+  resource :retirements, only: %i[new create]
 
   # イベントとチケットリソースは親子関係
-  resources :events do
-    resources :tickets
+  resources :events, only: %i[new create show edit update destroy] do
+    resources :tickets, only: %i[new create destroy]
   end
 
   get "/status" => "status#index", defaults: { format: "json" }
+  
+  match "*path" => "application#error404", via: :all
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
